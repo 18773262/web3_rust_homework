dd9c835d24cb36c199a044b3f7cd60f7102eef5a
WWQ <18773262@qq.com>
2023-10-22 16:57:58 +0800

    remove

--- lession_1/target/debug/deps/lession_1.d
+++ lession_1/target/debug/deps/lession_1.d
@@ -1,8 +1,8 @@
-C:\dev_rust\web3_rust_homework\lession_1\target\debug\deps\lession_1.exe: src\main.rs src\first_loop.rs src\second_loop\mod.rs src\second_loop\annother_loop.rs
+C:\dev_rust\web3_rust_homework\lession_1\target\debug\deps\lession_1.exe: src\main.rs src\first.rs src\second\mod.rs src\second\annother.rs
 
-C:\dev_rust\web3_rust_homework\lession_1\target\debug\deps\lession_1.d: src\main.rs src\first_loop.rs src\second_loop\mod.rs src\second_loop\annother_loop.rs
+C:\dev_rust\web3_rust_homework\lession_1\target\debug\deps\lession_1.d: src\main.rs src\first.rs src\second\mod.rs src\second\annother.rs
 
 src\main.rs:
-src\first_loop.rs:
-src\second_loop\mod.rs:
-src\second_loop\annother_loop.rs:
+src\first.rs:
+src\second\mod.rs:
+src\second\annother.rs:

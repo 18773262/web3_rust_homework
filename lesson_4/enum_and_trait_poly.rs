3bfe24897156623c417afe8a3d816bf6cdef4dfa
WWQ <18773262@qq.com>
2023-11-12 23:04:03 +0800

    fix

--- lesson_4/enum_and_trait_poly.rs
+++ lesson_4/enum_and_trait_poly.rs
@@ -1,3 +1,9 @@
+
+/*
+使用枚举包裹三个不同的类型，并放入一个Vec中，对Vec进行遍历，调用三种不同类型的各自的方法。
+定义三个不同的类型，使用Trait Object，将其放入一个Vec中，对Vec进行遍历，调用三种不同类型的各自的方法。
+同时，说明其上两种不同实现方法的区别
+ */
 fn vec_enum_poly() {
     struct Circle;
     struct Triangle;

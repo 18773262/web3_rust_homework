6f2cdc2d0d0030b91e010e15e92fb0c13f30bea4
WWQ <18773262@qq.com>
2023-11-12 20:39:25 +0800

    append

--- lession_1/src/main.rs
+++ lession_1/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     println!("Lesson 1");
     first::loop_a_uz();
     second::annother::loop_ua_z();
+
+    second::annother::from_str_to_array();
+    second::annother::print_ascii_start_with_a();
+
 }
--- lession_1/src/second/annother.rs
+++ lession_1/src/second/annother.rs
@@ -4,3 +4,34 @@ pub fn loop_ua_z() {
         println!("{ch}");
     }
 }
+pub fn print_ascii_start_with_a() {
+    let start: u8 = b'A';
+    let total_rows = 15;
+
+    for row in 0..total_rows {
+        for col in 0..4 {
+            let index = (row + col * total_rows) as u8;
+            let ascii_value = start + index;
+            print!("{:<4}{:<5}｜", ascii_value, ascii_value as char);
+        }
+        println!();
+    }
+}
+
+pub fn from_str_to_array() {
+    let s = "你好世界 Hello, world!";
+    let chars: Vec<char> = s.chars().collect(); println!("vec: {chars:?}");
+
+    let length =  s.chars().count();
+    println!("length: {length}");
+
+    let mut v = Vec::with_capacity(length);
+
+    for (idx, item) in s.chars().enumerate() { v[idx] = item;
+    }
+    println!("array: {v:?}");
+
+    let mut iter = v.iter().enumerate();
+    assert_eq!(Some((0, &'你')), iter.next());
+}
+
